46054b8d6597a2060350f1d59655c5b4fc033637
adel otma <adelotman1987@gmail.com>
2024-05-25 11:57:25 +0300

    fix the url

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,12 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe'],
-  ignoredRoutes: ["/no-auth-in-this-route"],
+
 });
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
+
+//"/anyone-can-visit-this-route"
